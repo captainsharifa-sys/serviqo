ade351f7c818022e745591b35ee823d04b736d0e
captainsharifa-sys <captainsharifa@gmail.com>
2026-08-01 17:03:12 +0300

    Initial Serviqo project with authentication and dashboard

---
+++ accounts/views.py
@@ -0,0 +1,21 @@
+from django.contrib.auth.views import LoginView
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+
+def signup(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            return redirect("/")
+
+    else:
+        form = UserCreationForm()
+
+    return render(request, "accounts/signup.html", {
+        "form": form
+        
+    })
+class UserLoginView(LoginView):
+    template_name = "accounts/login.html"
