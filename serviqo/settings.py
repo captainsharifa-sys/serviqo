eb34c9613ec870dfdbcdbbc3370bf269c5e2b2dd
Sharif Muhsin <captainsharifa@gmail.com>
2026-08-06 13:32:35 +0300

    Build booking system and dashboard features

--- serviqo/settings.py
+++ serviqo/settings.py
@@ -119,6 +119,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+# Static files (CSS, JavaScript, Images)
+
+STATIC_URL = "static/"
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
 LOGIN_REDIRECT_URL = "/dashboard/"
 LOGOUT_REDIRECT_URL = "/"
